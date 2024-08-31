0277338f756e36c8e8b4f7ccdec4dd70e4ad4dc1
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-08-31 16:28:57 -0600

    tailwind working

--- web/src/routes/error-page.tsx
+++ web/src/routes/error-page.tsx
@@ -1,3 +1,18 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
 export const ErrorPage = () => {
-  return <div>Error Page</div>;
+  const error = useRouteError();
+  console.log(error);
+
+  if (!isRouteErrorResponse(error)) {
+    return <div>Unknown error</div>;
+  }
+
+  return (
+    <div className="text-center text-red-700">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>{/* <i>{error.statusText || error.error?.message}</i> */}</p>
+    </div>
+  );
 };
