adc351d01b5626f49be826aa4ebbfaa2803bfae9
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-08-31 18:37:22 -0600

    cant get tailwind to work correctly

--- web/src/routes/signup-page.tsx
+++ web/src/routes/signup-page.tsx
@@ -1,18 +1,17 @@
 export const SignUpPage = () => {
   return (
     <div>
-      {" "}
       <div className="m-4 mb-10 text-center text-3xl font-bold">
         <h1>
           Welcome to{" "}
           <span className="text-orange-600">The Pattern's Space</span>
         </h1>
-        <h2>Sign up bellow to follow our progress</h2>
+        <h2>Sign up below to follow our progress</h2>{" "}
       </div>
       <div className="flex flex-col text-center justify-center">
         <div className="flex mt-1 justify-center items-end">
           <div className="text-gray-500 text-sm test-start mb-1 text-opacity-70">
-            Sign up with you email address
+            Sign up with your email address {/* "you" corrected to "your" */}
           </div>
         </div>
       </div>
